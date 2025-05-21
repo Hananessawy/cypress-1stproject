7a204d6cb8e4ac28e005a123e0b0f8596cefb877
Hazem Khaled <hazem.khaled@gmail.com>
2025-05-21 14:14:46 +0300

    Add cypress/videos to .gitignore and enable video recording in Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://salahub.ninja',
     supportFile: false,
+    video: true, // Enable video recording
+    videoCompression: 32, // Compression quality (lower = smaller file size)
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
